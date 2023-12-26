8e9714e0735a8fb57dc18796d69954e99b75e6d4
PurnimaAgnihotri <151939859+PurnimaAgnihotri@users.noreply.github.com>
2023-12-26 19:45:53 +0530

    Delete size.c

--- size.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>>
-void main(){
-    int a ;
-    float b;
-    printf("\n size of integer %d bytes",sizeof(int));
-    printf("\n size of float %d bytes",sizeof(float));
-    printf("\n size of a %d bytes",sizeof(a));
-    printf("\n size of b %d bytes",sizeof(b));
-}
