4938236d0545fae354bd66bfb2d7fe69cbfc112e
PurnimaAgnihotri <151939859+PurnimaAgnihotri@users.noreply.github.com>
2023-12-26 19:36:43 +0530

    Delete relationoperator.c

--- relationoperator.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-void main(){
-    int a,b;
-    printf(" \n enter two number:");
-    scanf("%d%d, &a ,&b");
-    printf("\na>b is = %d,a>b");
-    printf("\na<b is =%d",a<b);
-    
-}
